b7abcb4f3ec26200c6f468f1671aebae17f8299f
Jeff Taggart <jeff@jetaggart.com>
2015-02-14 16:23:36 -0700

    Add persistence module

---
+++ src/ZoeticSpace/Persistence.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE OverloadedStrings  #-}
+
+module ZoeticSpace.Persistence where
+
+import qualified Data.HashMap.Lazy as M
+import qualified Data.Text as T
+
+import Database.Neo4j
+
+port :: Port
+port = 7474
+
+host :: Hostname
+host = "localhost"
+
+-- | Dummy properties
+someProperties :: Properties
+someProperties = M.fromList [ "name" |: ("John" :: T.Text)
+                            , "email" |: ("j@j.com" :: T.Text)
+                            ]
+
+create = withConnection host port $ do
+   createNode someProperties
