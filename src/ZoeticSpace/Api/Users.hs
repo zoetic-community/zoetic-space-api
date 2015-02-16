e89fdc55868e7a073a5ac5e132c4210f56eb299d
Jeff Taggart <jeff@jetaggart.com>
2015-02-15 21:42:53 -0700

    Fix API to allow creation again without ID's
    * Beginning of data validations

--- src/ZoeticSpace/Api/Users.hs
+++ src/ZoeticSpace/Api/Users.hs
@@ -8,21 +8,32 @@ import Prelude hiding (id)
 import GHC.Generics (Generic)
 import Control.Applicative
 import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad
 import qualified Data.HashMap.Lazy as M
 import qualified Data.Text as T
 import qualified Data.ByteString as S
 
 import Web.Scotty (get, post, json, text, ScottyM, jsonData, ActionM)
-import Data.Aeson (FromJSON, ToJSON)
+import Data.Aeson (FromJSON, ToJSON, Value(Object), (.:), (.:?), parseJSON)
 import Database.Neo4j
 import Data.String.Conversions
 
 import ZoeticSpace.Persistence
 
-data User = User { id :: T.Text, name :: T.Text, email :: T.Text }
+type ValidationError = String
+
+data User = User { id :: Maybe T.Text, name :: T.Text, email :: T.Text }
             deriving (Show, Generic)
 
-instance FromJSON User
+instance FromJSON User where
+  parseJSON (Object v) =
+    User <$> v .:? "id"
+         <*> v .: "name"
+         <*> v .: "email"
+
+  
+  parseJSON _ = mzero
+  
 instance ToJSON User
 
 instance ToNeo4j User where
@@ -35,10 +46,18 @@ instance ToNeo4j User where
 instance FromNeo4j User where
   fromNode node = let properties = getNodeProperties node
                   in User {
-                            id = (cs . nodeId) node
+                            id = Just $ (cs . nodeId) node
                           , name = (getTextProperty "name" properties)
                           , email = (getTextProperty "email" properties)
                           }
+
+errorsFor :: User -> [ValidationError]
+errorsFor user = []
+
+createOrError successFunc entity = do
+  case errorsFor entity of
+    [] -> successFunc entity
+    errors -> json errors
 
 getUsers :: IO [User]
 getUsers = do
@@ -53,6 +72,10 @@ userRoutes = do
     
   post "/v1/users" $ do
     user <- jsonData :: ActionM User
-    node <- liftIO $ create user
-    json $ (fromNode node :: User)
+    createOrError persistUser user
+    
+  where persistUser user =  do
+          node <- liftIO $ create user
+          json $ (fromNode node :: User)
+
     
