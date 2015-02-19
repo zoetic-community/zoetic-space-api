5359bbd0eece00bab43fc08d30dc3aa7aa2e5699
Jeff Taggart <jeff@jetaggart.com>
2015-02-18 21:47:12 -0700

    Add simple test for /v1/users

--- tests/Main.hs
+++ tests/Main.hs
@@ -1,8 +1,8 @@
 {-# LANGUAGE OverloadedStrings, QuasiQuotes #-}
+
 module Main (main, spec) where
 
 import           Test.Hspec hiding (shouldContain)
-import           Test.QuickCheck
 import           Network.Wai.Test (SResponse)
 import           Data.ByteString (ByteString)
 import           Control.Applicative
@@ -17,20 +17,23 @@ main = hspec spec
 get :: ByteString -> IO SResponse
 get path = app >>= getPath path
 
+statusOk :: ByteString -> Expectation
+statusOk path = (statusCode <$> get path) `shouldReturn` 200
+
 spec :: Spec
 spec = do
   describe "GET /" $ do
     it "responds with HTTP status 200" $ do
-      (statusCode <$> get "/") `shouldReturn` 200
+      statusOk "/"
 
     it "says 'Hello!'" $ do
       (body <$> get "/") `shouldReturn` "Nothing to see here"
+  
+  describe "GET /v1/useres" $ do
+    it "responds with a 200" $ do
+      statusOk "/v1/users"
 
   context "when given an invalid request path" $ do
     it "responds with HTTP status 404" $ do
-      pending
+      (statusCode <$> get "/anything") `shouldReturn` 404
 
-  context "when given an *arbitrary* invalid request path" $ do
-    it "responds with HTTP status 404" $ do
-      property $
-        pending
