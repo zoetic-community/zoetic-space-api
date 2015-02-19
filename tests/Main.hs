9ae8191497331246aa18af4cd9dc02bf6d793854
Jeff Taggart <jeff@jetaggart.com>
2015-02-18 21:35:01 -0700

    Added CORS test for users

--- tests/Main.hs
+++ tests/Main.hs
@@ -14,3 +14,7 @@ spec = with app $ do
   describe "GET /" $ do
     it "responds with 200" $ do
       get "/" `shouldRespondWith` 200
+
+  describe "GET /v1/users" $ do
+    it "sets CORS header" $ do
+      get "/v1/users" `shouldRespondWith` 200 {matchHeaders = ["Access-Control-Allow-Origin" <:> "*"]}
